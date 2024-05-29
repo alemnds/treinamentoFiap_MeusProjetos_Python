85cbd7106109a3ce5ad2937769f416993c48c5c3
alemnds <alexandre.mendes1980@outlook.com>
2024-05-28 23:20:43 -0300

    Initial commit

---
+++ Capitulo2-Variaveis/Variaveis2.py
@@ -0,0 +1,6 @@
+responsavel=input("Digite o nome do responsavel: ")
+funcionario=input("Digite o nome do funcionarios: ")
+evento=input("Digite o nome do evento: ")
+valor=float(input("Digite o valor que será ressarcido: "))
+
+print("Declaro para o senhor " + responsavel + ", que o senhor " + funcionario + " esteve presente no evento " + evento + " e gastou o valor de R$ " + str(valor) + " com a entrada. " )
